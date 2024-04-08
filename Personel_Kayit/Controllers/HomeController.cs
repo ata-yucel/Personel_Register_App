47b2525d831b9faab0f9e275e8f26c67df68f795
Ata YÜCEL <atayucel184@gmail.com>
2024-04-08 21:23:17 +0300

    Proje dosyası ekle.

---
+++ Personel_Kayit/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personel_Kayit.Models;
+using System.Diagnostics;
+using System.IO;
+
+namespace Personel_Kayit.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Personel model)
+        {
+            StreamWriter writer = new StreamWriter("d://personel/personel.txt",true);
+            writer.WriteLine("Id : {0} ", model.Id);
+            writer.WriteLine("Ad : {0} ", model.Ad);
+            writer.WriteLine("Soyad : {0} ", model.Soyad);
+            writer.WriteLine("Memleket : {0} ", model.Memleket);
+
+            writer.WriteLine("-------------------------------------------");
+
+
+            writer.Close();
+
+            return View();
+
+        }
+
+        
+    }
+}
---
+++ Personel_Kayit/Models/Personel.cs
@@ -0,0 +1,11 @@
+﻿namespace Personel_Kayit.Models
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Memleket { get; set; }
+
+    }
+}
